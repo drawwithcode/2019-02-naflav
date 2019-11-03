b41d55089ebcfa25005871977a5d15696998253e
naflav <55958629+naflav@users.noreply.github.com>
2019-11-03 23:18:55 +0100

    Low Blood Pressure

--- sketch.js
+++ sketch.js
@@ -1,11 +1,35 @@
-function preload(){
-  // put preload code here
-}
-
 function setup() {
-  // put setup code h
+  createCanvas(windowWidth, windowHeight);
+
+
 }
 
 function draw() {
-  // put drawing code here
+  noStroke();
+
+
+  for (var x = 0; x < width; x += 10) {
+    for (var y = 0; y < height; y += 10) {
+
+      if (random() < 0.1) {
+        fill("#333333");
+      } else {
+        fill("#F87E61");
+      }
+
+      rect(x, y, x + 10, y + 10);
+
+
+    }
+  }
+
+  //title
+  textSize(20);
+  textAlign(CENTER);
+  fill("#F87E61");
+  stroke("#333333");
+  textFont("Roboto Mono");
+  text("low blood pressure time", width / 2, height / 2);
+
+
 }
